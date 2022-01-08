cbb91ba82640cded9bfee2a7931519645a3054c4
Nimesh Pareek <nimeshpareek25@gmail.com>
2022-01-08 19:03:03 +0530

    Project of day 1

---
+++ Day 1/Course_practice/Variables.py
@@ -0,0 +1,7 @@
+print("Nimesh")
+print("Pareek")
+
+# name = input() this also works fine
+name = input("Enter you friend name \n")
+length = len(name)
+print(length)
---
+++ Day 1/Project/Day_1_project.py
@@ -0,0 +1,9 @@
+print("Welcome to the band name generator")
+print("What's name of the city you grew up in")
+name_city = input()
+
+print("What's your pet name")
+name_pet = input()
+
+# print("Your band name could be", name_city + " " + name_pet)
+print("Your band name could be " + name_city + " " + name_pet)
