4618da9c07eb0c6640d165803ebdb8e5a6710f61
Nimesh Pareek <nimeshpareek25@gmail.com>
2022-01-11 22:05:15 +0530

    edited project of day 3

--- Day 3/Project/finding_game.py
+++ Day 3/Project/finding_game.py
@@ -1,35 +1,43 @@
-print (
+print(
     '''
-                                                                        
-    |||||||||8b,dPPYba,  ,adPPYba, ,adPPYYba, ,adPPYba, 88       88 8b,dPPYba,  
-       88    88P'   "Y8 a8P_____88 ""     `Y8 I8[    "" 88       88 88P'   "Y8  
-       88    88         8PP""""""" ,adPPPPP88  `"Y8ba,  88       88 88          
-       88,   88         "8b,   ,aa 88,    ,88 aa    ]8I "8a,   ,a88 88          
-       "Y888 88          `"Ybbd8"' `"8bbdP"Y8 `"YbbdP"'  `"YbbdP'Y8 88          
+
+    |||||||||8b,dPPYba,  ,adPPYba, ,adPPYYba, ,adPPYba, 88       88 8b,dPPYba,
+       88    88P'   "Y8 a8P_____88 ""     `Y8 I8[    "" 88       88 88P'   "Y8
+       88    88         8PP""""""" ,adPPPPP88  `"Y8ba,  88       88 88
+       88,   88         "8b,   ,aa 88,    ,88 aa    ]8I "8a,   ,a88 88
+       "Y888 88          `"Ybbd8"' `"8bbdP"Y8 `"YbbdP"'  `"YbbdP'Y8 88
                                                                  '''
 )
-name = input ("Please Enter your Name : ")
-upper_name = name.upper ()
-print (f"HEY {upper_name} WELCOME TO TREASURE HUNT GAME: ")
+name = input("Please Enter your Name : ")
+upper_name = name.upper()
+print(f"HEY {upper_name} WELCOME TO TREASURE HUNT GAME: ")
 
-print ("Your mission is to find a treasure in this game :)) ")
-choice1 = input (
+print("Your mission is to find a treasure in this game :)) ")
+choice1 = input(
     "you are at a crossroad where you want to go now if left then type left or is right type right :))>>"
-).lower ()
+).lower()
 
 if choice1 == "left":
-    choice2 = input (
+    choice2 = input(
         "You have come to a lake. There is an island in the middle of the lake."
-        " Type wait to wait for the boat: >> or type swim to swim across"
-    ).lower ()
-if choice2 == "wait":
-    choice3 = input (
+        " Type wait to wait for the boat: >> "
+    ).lower()
+    if choice2 == "wait":
+        choice3 = input(
         "You arrived at an island unharmed. There is a house with 3 doors. one "
         "red one yellow and one blue so choose one  >> "
-    ).lower ()
-if choice3 == "red":
-    print ("It's a room full of fire. Game over")
-elif choice3 == "yellow":
-    print ("You found the treasure. YOU WIN!!! HUREE!!!")
-elif choice3 == "blue":
-    print ("You enter a room of tiger, Game Over")
+         ).lower()
+        if choice3 == "red":
+             print("It's a room full of fire, Gmae Over!!!")
+        elif choice3 == "Yellow":
+             print("You found the treasure, YOU WIN!!!")
+        elif choice3 == "blue":
+            print("You enter the room of beasts. Game Over!!!")
+        else:
+            print("You chose the door which is close")
+    else:
+        print("You entered wrong input >>>>")
+else:
+    print("You fall into a hole. You LOSE~~~!!")       
+
+
