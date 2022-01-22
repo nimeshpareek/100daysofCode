eff3f6d1eb099c9fb8488d888dda3af164eff3d6
Nimesh Pareek <nimeshpareek25@gmail.com>
2022-01-22 23:11:30 +0530

    day 8 project

--- Day 8/Project/ceaser_cipher.py
+++ Day 8/Project/ceaser_cipher.py
@@ -7,27 +7,6 @@ plain_text = input("Type your message:\n").lower()
 shifted_value = int(input("Type the shift number:\n"))
 
 
-# TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-
-# TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
-# e.g.
-# plain_text = "hello"
-# shift = 5
-# cipher_text = "mjqqt"
-# print output: "The encoded text is mjqqt"
-
-##HINT: How do you get the index of an item in a list:
-# https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-# def encrypt(your_text, i):
-#     for i in alphabet:
-#         [i] = alphabet;
-
-
-# TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
-# encrypt(your_text = text, i = shift)
-
 def encrypt(text, shift):
     cipher_text = ""
     for letter in text:
